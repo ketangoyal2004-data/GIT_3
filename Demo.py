6dd15030411e941d114ef34d5cd03f93e30e8654
ketangoyal2004-data <ketangoyal858@gmail.com>
2026-02-02 09:00:37 +0530

    This is added

--- Demo.py
+++ Demo.py
@@ -15,8 +15,12 @@ class std:
         self.age = age
         return self.age
 
+    def city(self,city):
+        self.city = city
+        return self.city
 obj = std()
 print(obj.name("Alice"))  
 print(obj.course("Mathematics"))
 print(obj.branch("Sci"))
 print(obj.Age(21))
+print(obj.city("New York"))
--- Demo2.py
+++ Demo2.py
@@ -11,6 +11,9 @@ class cal:
     def div(self,a,b):
         return a / b
     
+    def exp(self,a,b):
+        return a ** b
+    
 obj = cal() 
 obj.add(10,20)
 obj.sub(20,10)
