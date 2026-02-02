e912d7f6d41e9603d7b982d4fd777b5437f8d4cf
ketangoyal2004-data <ketangoyal858@gmail.com>
2026-02-02 08:57:51 +0530

    This is a new files added

--- Demo.py
+++ Demo.py
@@ -6,7 +6,17 @@ class std:
     def course(self,course):
         self.course = course
         return self.course  
+    
+    def branch(self,branch):
+        self.branch = branch
+        return self.branch  
+    
+    def Age(self,age):
+        self.age = age
+        return self.age
 
 obj = std()
 print(obj.name("Alice"))  
 print(obj.course("Mathematics"))
+print(obj.branch("Sci"))
+print(obj.Age(21))
--- Demo2.py
+++ Demo2.py
@@ -4,7 +4,13 @@ class cal:
     
     def sub(self,a,b):
         return a - b
+    
+    def mul(self,a,b):
+        return a * b
 
+    def div(self,a,b):
+        return a / b
+    
 obj = cal() 
 obj.add(10,20)
 obj.sub(20,10)
